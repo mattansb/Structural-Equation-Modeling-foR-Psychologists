4ac3af26b6591d56b66306ef993f95f97be80f36
mattansb <35330040+mattansb@users.noreply.github.com>
2020-03-03 17:58:09 +0200

    Update advanced plotting.R

--- 01 path analysis/advanced plotting.R
+++ 01 path analysis/advanced plotting.R
@@ -63,7 +63,7 @@ semPaths(fit,
          layout = m)
 
 # For a shiny app that can help, see
-# https://mattansb.github.io/MSBMisc/reference/semPaths_layout_maker.html
+# https://mattansb.github.io/MSBMisc/reference/node_layout_maker.html
 
 
 # Final product -----------------------------------------------------------
