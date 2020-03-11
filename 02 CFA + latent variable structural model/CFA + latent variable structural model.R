598ca7ecf38f8c9c5af14b573a52a3a736b4a799
mattansb <35330040+mattansb@users.noreply.github.com>
2020-03-11 17:41:51 +0200

    re-structure lesson 2

--- 02 CFA + latent variable structural model/CFA + latent variable structural model.R
+++ 02 CFA + latent variable structural model/CFA + latent variable structural model.R
@@ -68,37 +68,12 @@ mod_lat_only <- '
 fit_lat_only <- cfa(mod_lat_only, data = adhd_anx)
 
 semTools::reliability(fit_lat_only) # see also semTools::reliabilityL2
-# read about the indices here:
+# We will look at `omega`, which is similar to alpha, but doesn't assume
+# equal weights / loadings (which we just estimated!). It can be thought of
+# as representing the variance explained across the indicators of each factor.
+# https://doi.org/10.1037/met0000144
+# read about the other indices here, and when you'd like to use them:
 ?semTools::reliability
-
-# Modification Indices ----------------------------------------------------
-
-# We can look at modification indices (mi):
-modificationIndices(fit_meas, sort. = TRUE, maximum.number = 5)
-modificationIndices(fit_meas, sort. = TRUE, minimum.value = 10)
-# what should we do?
-
-
-
-# Fit Measures ------------------------------------------------------------
-
-fitMeasures(fit_meas, output = "matrix",
-            fit.measures = c(
-              "chisq", "df","pvalue",
-              "baseline.chisq","baseline.df","baseline.pvalue"
-            ))
-# The "baseline" model is the null /"independence" model, which constraints
-# all covariances to zero, AKA the worst model.
-
-
-fitMeasures(fit_meas, output = "matrix",
-            fit.measures = c("nfi","nnfi","tli", "cfi","rmsea"))
-# What we want:
-# Should Chi-sq be sig?
-#        NFI > 0.9
-# NNFI / TLI > 0.9
-#        CFI > 0.9
-#      RMSEA < 0.08
 
 # 2. structural model -----------------------------------------------------
 
@@ -137,43 +112,154 @@ semPaths(fit_struct, what = "std", whatLabels = "std",
 summary(fit_struct, standardize = TRUE)
 
 
-# Fit Measures ------------------------------------------------------------
+
+# Measures of Fit ---------------------------------------------------------
+# and model comparisons
 
 fitMeasures(fit_struct, output = "matrix",
             fit.measures = c(
               "chisq", "df","pvalue",
               "baseline.chisq","baseline.df","baseline.pvalue"
             ))
+# The "baseline" model is the null /"independence" model, which constraints
+# all covariances to zero, AKA the worst model.
 
-# We can give another baseline model
+# But we can give another baseline model
 fitMeasures(fit_struct, output = "matrix",
             fit.measures = c(
               "chisq", "df","pvalue",
               "baseline.chisq","baseline.df","baseline.pvalue"
             ),
             baseline.model = fit_meas)
+
+# To actually compare twho (nested!) models:
 anova(fit_struct, fit_meas) # Is this good or bad?
+
 bayestestR::bayesfactor_models(fit_struct, denominator = fit_meas)
+# Bayes factors can also be used to compare non-nested models...
 # (You can also fit a true Bayesian lavaan model with `blavaan`)
+
+
 
 
 
 fitMeasures(fit_struct, output = "matrix",
             fit.measures = c("nfi","nnfi","tli", "cfi","rmsea"))
-# But some of these are RELATIVE measures!
+# What we want:
+#        NFI > 0.9
+# NNFI / TLI > 0.9
+#        CFI > 0.9
+#      RMSEA < 0.08
 
-# We must give the model to which we want to compare:
+# But (except for rmsea) these are RELATIVE measures - by default
+# they are relative to the null /"independence" model,
+# But we can give another baseline model:
 fitMeasures(fit_struct, output = "matrix",
             fit.measures = c("nfi","nnfi","tli", "cfi","rmsea"), 
             baseline.model = fit_meas) # note we added a NEW baseline!
 
 
+
+
+
+
+
+
+
+
+# 3. Constraining and Unconstraining --------------------------------------
+
+# This is where the magic happens!
+# AKA hypothesis testing!
+
+
+# Constraining ------------------------------------------------------------
+
+## Example 1) equal loadings on a factor:
+
+mod_meas_eq_load <- '
+  ## latent variable definitions (CFA)
+  ## The "=~" can be read as "is identified by"
+  ADHD =~ a * adhd1 + a * adhd2 + a * adhd3
+    IQ =~ iq1 + iq2
+   ANX =~ anx1 + anx2 + anx3 
+  
+  ## self-regression
+  sustained_attn ~ 1 * sustained_attn
+      inhibition ~ 1 * inhibition
+  # We need to define one of the observed variables (this is a silly bug).
+  # This does not affect the model fit!
+  
+  ## covariances
+  sustained_attn ~~ inhibition + ADHD + IQ + ANX
+      inhibition ~~ ADHD + IQ + ANX
+'
+
+fit_meas_eq_load <- cfa(mod_meas_eq_load, data = adhd_anx, 
+                        std.lv = TRUE)
+summary(fit_meas_eq_load, standardize = TRUE)
+# (Note that the standardized loadings are NOT equal!
+# We will learn next time how to deal with that..)
+
+
+
+## Example 2) set a path to 0:
+# We did this! That is what the structural model is!
+mod_struct_eq_load <- '
+  ## latent variable definitions
+  ADHD =~ a * adhd1 + a * adhd2 + a * adhd3
+    IQ =~ iq1 + iq2
+   ANX =~ anx1 + anx2 + anx3 
+  
+  ## regressions
+      inhibition ~ ADHD + ANX + IQ
+  sustained_attn ~ inhibition + ADHD
+  
+  ## covariances
+  IQ ~~ ADHD      # we dont need this, because the of defaults...
+  IQ ~~ 0 * ANX
+'
+# Which paths have been constrained to 0?
+# (we can constrain to any value we'd like!)
+
+fit_struct_eq_load <- sem(mod_struct_eq_load, data = adhd_anx,
+                          std.lv = TRUE)
+
+# Measures of Fit ---------------------------------------------------------
+# and model comparisons
+
+anova(fit_meas, fit_meas_eq_load, fit_struct_eq_load)
+bayestestR::bayesfactor_models(fit_meas_eq_load, fit_struct_eq_load,
+                               denominator = fit_meas)
+# Good? Bad?
+
+fitMeasures(fit_meas_eq_load, output = "matrix",
+            fit.measures = c("nfi","nnfi","tli", "cfi","rmsea"), 
+            baseline.model = fit_meas)
+
+fitMeasures(fit_struct_eq_load, output = "matrix",
+            fit.measures = c("nfi","nnfi","tli", "cfi","rmsea"), 
+            baseline.model = fit_meas_eq_load)
+
+# Modification Indices ----------------------------------------------------
+
+
+# We can look at modification indices (mi):
+modificationIndices(fit_meas, sort. = TRUE, maximum.number = 5)
+modificationIndices(fit_meas, sort. = TRUE, minimum.value = 10)
+# what should we do?
+
+#' why `iq2 ~~ anx1`?
+
+# What would our next steps be?
+
+
 # Exercise ----------------------------------------------------------------
 
 library(dplyr)
-big5 <- psychTools::bfi %>% 
+big5 <- psychTools::bfi %>%
   select(A1:A5, C1:C5, E1:E5, N1:N5, O1:O5,
-         gender, age) %>% 
+         gender, age) %>%
   na.omit()
 
 head(big5)
@@ -181,6 +267,6 @@ head(big5)
 # 1. Create a measurment model with the big 5 latent variables, age, and gender.
 #    - Explore the MIs. Is there anything you should add? Does it make sense?
 # 2. Create 2 structural models of your choice with the big5, age and gender.
-#    - Compare them (sig, BF).
+#    - Compare them (sig [make sure they are nested], BF).
 #    - Compare their measures of fit.
-
+# 3. Plot one of the 3 models from 1-2.
