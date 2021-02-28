7f495116435bf5529c86938f4f310232be425543
mattansb <35330040+mattansb@users.noreply.github.com>
2021-02-28 09:10:18 +0200

    re-do Class 1

--- 01 path analysis/path analysis.R
+++ 01 path analysis/path analysis.R
@@ -1,29 +1,40 @@
 
+# We will be using a simple mediation model to demonstrate the principals of
+# path analysis.
+
 income_psych <- read.csv("income_psych.csv")
 
 head(income_psych)
 
-# Testing mediation:
+# Our question:
 # Does negative mood mediate the relationship between anxiety and income?
 # Graphically, it looks like "plot1.png"
 
+
+
+
+
 # Manual path analysis ----------------------------------------------------
 
-m1 <- lm(income ~ anxiety + mood_neg, income_psych)
-m2 <- lm(mood_neg ~ anxiety, income_psych)
+m1 <- lm(income ~ anxiety + neg_mood, income_psych)
+m2 <- lm(neg_mood ~ anxiety, income_psych)
 
+# We can "build" the paths by multiplying the correct coefficiants:
 (coef1 <- coef(m1))
 (coef2 <- coef(m2))
 
-direct <- unname(coef1[2])
-indirect <- unname(coef1[3] * coef2[2])
+direct <- unname(coef1['anxiety'])
+indirect <- unname(coef1['neg_mood'] * coef2['anxiety'])
 
-mediation_by_hand <- c(direct = direct,
-                       indirect = indirect,
-                       total = direct + indirect)
+(mediation_by_hand <- c(direct = direct,
+                        indirect = indirect,
+                        total = direct + indirect))
 
-summary(lm(income ~ anxiety, income_psych))
-mediation_by_hand
+# Is the total correct?
+lm(income ~ anxiety, data = income_psych)
+
+
+
 
 
 # With lavaan -------------------------------------------------------------
@@ -32,31 +43,54 @@ mediation_by_hand
 
 library(lavaan)
 
-# modelss are written as a multi-line charachter object:
+# model specification are written as a multi-line character:
 mediation_model <- '
-    income ~ anxiety + mood_neg
-  mood_neg ~ anxiety
+  neg_mood ~ anxiety
+    income ~ anxiety + neg_mood
 '
 
 # fit the model to the data:
 fit <- sem(mediation_model, data = income_psych)
 
-summary(fit) # (why is the Test Statistic 0?)
+
+## Model summary
+summary(fit) # get estimates + tests statistics
+# Note that by DEFAULT, lavaan estimates all residual errors.
+# (why is the Test Statistic 0?)
 
 summary(fit, standardize = TRUE) # look at Std.all for beta
-summary(fit, rsquare = TRUE) # See regression R2
-# Note that by DEFAULT, lavaan estimates all residual errors.
+lavInspect(fit, what = "rsquare") # See regression R2
 
 
-# Modifiers! --------------------------------------------------------------
+## Confidence intervals:
+parameterestimates(fit, ci = TRUE, output = "text") # ci for the raw estimates
+standardizedSolution(fit, ci = TRUE, output = "text") # ci for the std estimates
+# Note that the tests from these 2 functions CAN BE DIFFERENT (see the
+# z-values). Why?
 
-# how to compute the paths? With *modifiers*!
-# - We can mark parameters with a "stand-ins" followed by "*"
-# - We can have lavaan compute effects with ":="
+
+## Bootstrap
+fit_with_boot <- sem(mediation_model, data = income_psych,
+                     se = "bootstrap", bootstrap = 200)
+summary(fit_with_boot, standardize = TRUE)
+
+
+
+
+
+
+## Modifiers! ------------
+
+# - We can mark parameters with a "stand-ins" followed by "*" - these stand-ins
+#   are called *modifiers*.
+# - We can have lavaan compute different estimates with ":="
+#
+# This allows up to estimate paths in lavaan:
+
 mediation_model <- '
   # regressions
-  mood_neg ~ a * anxiety
-    income ~ b * mood_neg + c * anxiety
+  neg_mood ~ a * anxiety
+    income ~ b * neg_mood + c * anxiety
   
 
   # effects
@@ -66,27 +100,26 @@ mediation_model <- '
 '
 fit <- sem(mediation_model, data = income_psych)
 
+
 summary(fit, standardize = TRUE) # look at "Defined Parameters" 
 mediation_by_hand
 cor(income_psych$anxiety, income_psych$income)
 
-## Confidence intervals:
-summary(fit, standardize = TRUE, ci = TRUE) # ci for the raw estimates
-standardizedSolution(fit, ci = TRUE, output = "text") # ci for the std estimates
-
-## Bootstrap
-fit_with_boot <- sem(mediation_model, data = income_psych,
-                     se = "bootstrap", bootstrap = 200)
-summary(fit_with_boot, standardize = TRUE)
 
 
-# Compare paths -----------------------------------------------------------
 
-# We can also compare paths directly:
+
+
+## Compare paths --------
+
+# With the ":=" operator, we can estimate many things!
+# E.g., we can compare paths:
+
+
 mediation_model <- '
   # regressions
-  mood_neg ~ a * anxiety
-    income ~ b * mood_neg + c * anxiety
+  neg_mood ~ a * anxiety
+    income ~ b * neg_mood + c * anxiety
   
 
   # effects
@@ -96,56 +129,57 @@ mediation_model <- '
       
   path_diff := direct - indirect
 '
+
 fit <- sem(mediation_model, data = income_psych)
 
 # look at "Defined Parameters" 
 parameterEstimates(fit, output = "text") # summary gives these test values
 standardizedSolution(fit, output = "text")
-# Note that the tests from these 2 functions CAN BE DIFFERENT. Why?
+
+
+
 
 
 fit <- sem(mediation_model, data = income_psych,
            likelihood = "wishart")
 # likelihood = "wishart" used an unbiased cov-matrix, and gives
 # similar results to AMOS (SPSS).
-# In large samples this heardly has any effects...
-# Read more about the varouls likelihoods and estimators:
+# In large samples this hardly has any effects...
+# Read more about the various likelihoods and estimators:
 # http://lavaan.ugent.be/tutorial/est.html
 
-# Plotting ----------------------------------------------------------------
 
-library(semPlot)
-# very (too?) customisable:
-semPaths(fit)
-semPaths(fit, whatLabels = "est")
-semPaths(fit, whatLabels = "std")
 
-?semPaths # there are a million options!
 
-semPaths(fit, what = "std", whatLabels = "std", 
-         residuals = TRUE, intercepts = FALSE,
-         # prettify
-         style = "lisrel", normalize = TRUE, 
-         sizeMan = 11, sizeMan2 = 7,
-         sizeLat = 11, sizeLat2 = 7,
-         nCharNodes = 50,
-         edge.label.cex = 1)
-# see `advanced plotting` for even more fine-tuning.
 
-library(lavaanPlot)
-# pretty out of the box, but very very BUGGY!
-lavaanPlot(model = fit)
-lavaanPlot(model = fit, coefs = TRUE)
+
+
+
+
+## Plotting --------
+
+library(tidySEM)
+
+graph_sem(fit)
+graph_sem(fit, label = "est")
+graph_sem(fit, label = "est_std")
 
 
 # See also advanced plotting.R
 
 
+
+
+
 # Exercise ----------------------------------------------------------------
 
-# 1. Look at the plot in "plot2.png". Fit this model with `lavaan`.
+# 1. Look at the plot in "plot2.png". 
+#   - Explain the causal relationship in this model (in words).
+#   - Fit this model with `lavaan`.
 # 2. Compute all the paths in this model from *anxiety* to *income*, and 
-#    the total of these paths.
-#    - Why is the std total not equal exactly to the real correlation?
-#    - is it very different?
+#    the total of these paths. Explain your findings.
+# 3. Why is the std total NOT equal *exactly* to the real correlation?
+#   - Is it very different? What does this mean?
+# 4. Compute the difference between the two indirect paths. 
+#   - How big is it? Is it significant?
 
