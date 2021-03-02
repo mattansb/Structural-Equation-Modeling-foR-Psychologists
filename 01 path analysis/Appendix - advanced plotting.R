7d078bcc2248b7e98bf836edcea59e920746944d
mattansb <35330040+mattansb@users.noreply.github.com>
2021-03-02 09:50:12 +0200

    use prepare_graph

--- 01 path analysis/Appendix - advanced plotting.R
+++ 01 path analysis/Appendix - advanced plotting.R
@@ -13,18 +13,6 @@ fit <- sem(mediation_model, data = income_psych)
 ## See below for how "plot1.png" and "plot2.png" were made.
 
 
-
-# Changing Nodes (Vars) ---------------------------------------------------
-
-(nods <- get_nodes(fit))
-
-nods <- nods %>% 
-  mutate(label = c("Negative\nMood", "Income", "Anxiety")) # \n is a break line
-
-graph_sem(fit,
-          nodes = nods)
-
-
 # Changing Layout ---------------------------------------------------------
 
 lay <- get_layout(
@@ -33,20 +21,55 @@ lay <- get_layout(
   rows = 2
 )
 
-graph_sem(fit,
-          nodes = nods, 
+graph_sem(fit, 
           layout = lay, angle = 90)
 
 
 
+# Changing Nodes (Vars) ---------------------------------------------------
+
+# Instead of directly plotting, we will use `prepare_graph()` as an intermediary
+# step.
+
+g <- prepare_graph(fit, 
+                   layout = lay, angle = 90)
+
+# we can now see and change node data:
+nodes(g)
+# as you can see, there are many things you can change here!
+# You can also add color, fill, size, linetype and alpha.
+
+
+nodes(g) <- nodes(g) %>% 
+  mutate(label = c("Anxiety", "Income", "Negative\nMood")) # \n is a break line
+
+plot(g)
+
+
 # Changing Edges (Paths) --------------------------------------------------
 
+# For edges, we first need to ask for some extra columns that have info about
+# the paths, with `get_edges()`:
 
 (edgs <- get_edges(fit,
                    columns = c("est_std", "confint_std", "pval_std"),
                    digits = 3))
 
-edgs <- edgs %>% 
+
+# we can now use this with prepare_graph:
+g <- prepare_graph(fit, 
+                   layout = lay, angle = 90,
+                   edges = edgs)
+
+
+
+# And as we did with the nodes, we can see and change edge data:
+
+edges(g)
+# as you can see, there are many things you can change here!
+# You can also add color, size, linetype and alpha.
+
+edges(g) <- edges(g) %>% 
   mutate(
     label = paste0(est_std, "\n", confint_std),
     color = "black",
@@ -56,10 +79,7 @@ edgs <- edgs %>%
     linetype = replace(linetype, as.numeric(pval_std) > 0.05, "dashed")
   )
 
-graph_sem(fit,
-          nodes = nods, 
-          layout = lay, angle = 90,
-          edges = edgs)
+plot(g)
 
 # There are many more options, you can read more here:
 # https://cjvanlissa.github.io/tidySEM/articles/Plotting_graphs.html
@@ -81,8 +101,8 @@ nods <- get_nodes(fit) %>%
   mutate(label = c("Negative\nMood", "Income", "Anxiety")) # \n is a break line
 
 lay <- get_layout(
-  NA,         "neg_mood", NA,
-  "anxiety", NA,        "income",
+  NA,        "neg_mood", NA,
+  "anxiety", NA,         "income",
   rows = 2
 )
 
