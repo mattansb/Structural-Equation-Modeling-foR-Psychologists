e28297ce3b659a987dae3092323d0a65d1beca54
mattansb <35330040+mattansb@users.noreply.github.com>
2021-03-02 16:22:43 +0200

    update: LGCM

--- 06 latent growth curve + multi-level SEM/latent growth curve modeling.R
+++ 06 latent growth curve + multi-level SEM/latent growth curve modeling.R
@@ -14,14 +14,16 @@
 # the indevidual differences overall, regardless of time (the "intercept").
 
 intimacy_depression <- read.csv("intimacy_depression.csv")
+head(intimacy_depression)
 
 # Lets see how depression changes over time, and how anxiety, verbal IQ, and
 # relationship intimacy are related.
 
+library(lavaan)
+library(tidySEM)
+
 
 # 1. Fit the latent growth curve ------------------------------------------
-
-library(lavaan)
 
 LGCM <- '
   # intercept and slope with ***fixed coefficients***
@@ -38,19 +40,14 @@ summary(fit_LGC, standardize = TRUE)
 # Note: The intercept of slope is significant, suggesting that over all there is
 # an effect for time.
 
-library(semPlot)
 
-semPaths(fit_LGC, what = "std", whatLabels = "std", 
-         residuals = TRUE, intercepts = FALSE,
-         # prettify
-         fade = FALSE,
-         style = "lisrel", normalize = TRUE, 
-         sizeMan = 15, sizeMan2 = 7,
-         sizeLat = 15, sizeLat2 = 7,
-         nCharNodes = 50,
-         edge.label.cex = 1.3,
-         edge.label.bg = FALSE, edge.label.color = "black",
-         edge.label.position = 0.45)
+lay <- get_layout(
+  "depression_t1", "depression_t2", "depression_t3", "depression_t4",
+  NA,              "intercept",     "slope",         NA,
+  rows = 2
+)
+
+graph_sem(fit_LGC, label = "est_std", layout = lay)
 
 
 
@@ -81,22 +78,15 @@ fit_LGC_S <- growth(LGCM_S, data = intimacy_depression,
 
 summary(fit_LGC_S, standardize = TRUE)
 
-semPaths(fit_LGC_S, whatLabels = "std", 
-         residuals = TRUE, intercepts = FALSE, exoCov = FALSE,
-         # prettify
-         fade = FALSE,
-         style = "lisrel", normalize = TRUE, 
-         sizeMan = 13, sizeMan2 = 7,
-         sizeLat = 11, sizeLat2 = 7,
-         nCharNodes = 50,
-         edge.label.cex = 1,
-         edge.label.bg = FALSE, edge.label.color = "black",
-         edge.label.position = 0.45)
 
 
 
-# Multi-level models ------------------------------------------------------
+lay <- get_layout(
+  "intimacy_t1",   "intimacy_t2",   "intimacy_t3",   "intimacy_t4",
+  "depression_t1", "depression_t2", "depression_t3", "depression_t4",
+  NA,              "intercept",     "slope",         NA,
+  "anxiety",       NA,              NA,              "verbal_IQ",
+  rows = 4
+)
 
-# Of intrest are also multi-level models (MLM, aka HLM / LMM). This are 
-# quite dificult and have only limited support:
-# http://lavaan.ugent.be/tutorial/multilevel.html
+graph_sem(fit_LGC_S, label = "est_std", layout = lay)
---
+++ 06 latent growth curve + multi-level SEM/multi-level SEM.R
@@ -0,0 +1,5 @@
+# Multi-level models ------------------------------------------------------
+
+# Of interest are also multi-level models (MLM, aka HLM / LMM). This are quite
+# difficult and have only limited support:
+# http://lavaan.ugent.be/tutorial/multilevel.html
